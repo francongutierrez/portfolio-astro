6e5e177c35a4e943300c246c1bddc1efcb4c318a
francongutierrez <francogutierrez04@gmail.com>
2025-10-21 11:23:49 -0300

    feat: Añade la funcionalidad del modo oscuro y cambia la estructura de los archivos

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // Habilita el modo oscuro
   content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
